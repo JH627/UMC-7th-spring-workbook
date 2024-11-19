ef03fddac80d1499c106293518d0f8e685d1d0ca
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-11-19 16:41:13 +0900

    ✨ feat: Api, Swagger, Annotation 구현 #1

--- spring/src/main/java/umc/study/domain/Member.java
+++ spring/src/main/java/umc/study/domain/Member.java
@@ -2,6 +2,9 @@ package umc.study.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import umc.study.domain.common.BaseEntity;
 import umc.study.domain.enums.Gender;
 import umc.study.domain.enums.MemberStatus;
@@ -16,6 +19,8 @@ import java.util.List;
 
 @Entity
 @Getter
+@DynamicUpdate
+@DynamicInsert
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -38,11 +43,6 @@ public class Member extends BaseEntity {
     @Column(columnDefinition = "VARCHAR(10)")
     private Gender gender;
 
-    @Column(nullable = false, length = 50)
-    private String email;
-
-    private Integer point;
-
     @Enumerated(EnumType.STRING)
     private SocialType socialType;
 
@@ -52,6 +52,11 @@ public class Member extends BaseEntity {
 
     private LocalDate inactiveDate;
 
+//    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("0")
+    private Integer point;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<MemberAgree> memberAgreeList = new ArrayList<>();
