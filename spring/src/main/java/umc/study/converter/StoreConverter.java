37f57e541169d8935818cd25dd1f0480fe38008e
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-11-26 14:19:19 +0900

    💫 [Feat] 가게의 리뷰 목록 조회 API 구현 #11

--- spring/src/main/java/umc/study/converter/StoreConverter.java
+++ spring/src/main/java/umc/study/converter/StoreConverter.java
@@ -1,7 +1,12 @@
 package umc.study.converter;
 
+import org.springframework.data.domain.Page;
+import umc.study.domain.Review;
 import umc.study.domain.Store;
 import umc.study.web.store.dto.StoreResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StoreConverter {
 
@@ -11,5 +16,28 @@ public class StoreConverter {
                 .createdAt(store.getCreatedAt())
                 .build();
 
+    }
+
+    public static StoreResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review) {
+        return StoreResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getBody())
+                .build();
+    }
+    public static StoreResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList) {
+
+        List<StoreResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(StoreConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return StoreResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
+                .build();
     }
 }
